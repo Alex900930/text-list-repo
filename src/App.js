f4f7e1cbed46dc2567fcd03da5a91d09f29557ff
Alexander Herrera <Alex>
2023-08-03 20:53:04 -0400

    First page finish

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import plusSquareIcon from './images/plus-square.svg';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="input-container">
+        <input
+          type="text"
+          placeholder="        Type to add new task"
+        />
+        <img src={plusSquareIcon} alt="Plus Square Icon" className="icon" />
+      </div>
     </div>
   );
 }
